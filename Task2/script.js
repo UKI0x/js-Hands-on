12fb88230263925e7f62172196cafba3a68251f6
uki0x <ono.oooo.syty@gmail.com>
2021-05-05 18:47:54 +0900

    [update]定数newListでinsertAdjacentHTML、定数newList02でprependの挙動を確認しました

--- Task2/script.js
+++ Task2/script.js
@@ -2,20 +2,33 @@
 	
 	//ulタグを取得
 	const newList = document.getElementById("js_list");
+	const newList02 = document.getElementById("js_list02");
 
 	//ulに入れる中身を作成
+	//newList
 	const li = document.createElement("li");
 	const a = document.createElement("a");
-	const img = document.createElement("img");
+	
+	//newList02
+	const li02 = document.createElement("li");
+	const a02 = document.createElement("a");
+	const img02 = document.createElement("img");
 
-	//タグの属性を編集
+	//aタグの属性を編集
+	//newList
 	a.href = "1.html";
 
-	img.src = "bookmark.png";
-	img.alt = "ブックマーク";
-	img.innerHTML = "これです";
+
+	//newList02
+	//aタグの属性を編集
+	a02.href = "1.html";
 	//aタグ直下にテキストを追加
 	a02.textContent = "これです";
+	//imgタグの属性を編集
+	img02.src = "bookmark.png";
+	img02.alt = "ブックマーク";
 	
 	//子要素を指定
-	newList.appendChild(li).appendChild(a).appendChild(img);
+	newList.appendChild(li).appendChild(a).insertAdjacentHTML('afterbegin','<img src="bookmark.png" alt="ブックマーク" />これです');
+
+	newList02.appendChild(li02).appendChild(a02).prepend(img02);
