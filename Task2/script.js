e2cb984b59fe134468eafa52c00f1bcfcd5aa83f
uki0x <ono.oooo.syty@gmail.com>
2021-05-05 20:26:56 +0900

    [update]insertAdjacentElementでの対応をnewList03で確認

--- Task2/script.js
+++ Task2/script.js
@@ -3,6 +3,7 @@
 	//ulタグを取得
 	const newList = document.getElementById("js_list");
 	const newList02 = document.getElementById("js_list02");
+	const newList03 = document.getElementById("js_list03");
 
 	//ulに入れる中身を作成
 	//newList
@@ -13,6 +14,12 @@
 	const li02 = document.createElement("li");
 	const a02 = document.createElement("a");
 	const img02 = document.createElement("img");
+
+	//newList03
+	//newList02
+	const li03 = document.createElement("li");
+	const a03 = document.createElement("a");
+	const img03 = document.createElement("img");
 
 	//aタグの属性を編集
 	//newList
@@ -27,8 +34,19 @@
 	//imgタグの属性を編集
 	img02.src = "bookmark.png";
 	img02.alt = "ブックマーク";
+
+	//newList03
+	//aタグの属性を編集
+	a03.href = "1.html";
+	//aタグ直下にテキストを追加
+	a03.textContent = "これです";
+	//imgタグの属性を編集
+	img03.src = "bookmark.png";
+	img03.alt = "ブックマーク";
 	
 	//子要素を指定
 	newList.appendChild(li).appendChild(a).insertAdjacentHTML('afterbegin','<img src="bookmark.png" alt="ブックマーク" />これです');
 
 	newList02.appendChild(li02).appendChild(a02).prepend(img02);
+
+	newList03.appendChild(li03).appendChild(a03).insertAdjacentElement("afterbegin", img03);
