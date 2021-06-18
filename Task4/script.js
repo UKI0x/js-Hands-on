ddc9b4cac73e1b8d3b94ed5f870b78f3d2d399fd
uki0x <ono.oooo.syty@gmail.com>
2021-06-18 22:45:17 +0900

    [refactor] アロー関数として修正

--- Task4/script.js
+++ Task4/script.js
@@ -3,17 +3,16 @@
 
 const ul = document.getElementById("ul");
 const fragment = document.createDocumentFragment();
-const liLength　= 2;
 
 const liArray = [
-	{to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
-	{to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+	{ to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
+	{ to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" }
 ];
 
-liArray.forEach(function(list){
-	const li = document.createElement("li");
-	const a = document.createElement("a");
-	const img = document.createElement('img');
+liArray.forEach(list => {
+	let li = document.createElement("li");
+	let a = document.createElement("a");
+	let img = document.createElement('img');
 
 	a.href = `/${list.to}`;
 	a.textContent = list.text;
@@ -21,6 +20,6 @@ liArray.forEach(function(list){
 	img.alt = list.alt;
 
 	fragment.appendChild(li).appendChild(a).prepend(img);
-})
+});
 
 ul.appendChild(fragment);
